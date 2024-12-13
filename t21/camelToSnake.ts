466b8458ec062d9b1b6555d754dc5a4e614444cd
Daniel Nagel <danielnagel@mailbox.org>
2024-12-13 17:17:28 +0100

    read multiple files

--- t21/camelToSnake.ts
+++ t21/camelToSnake.ts
@@ -1,18 +1,28 @@
 (async () => {
-    let textFileContent = '';
+  for (const fileName of Deno.args) {
+    let textFileContent = "";
     try {
-        textFileContent = await Deno.readTextFile(Deno.args[0]);
-    } catch(e: unknown) {
-        if(e instanceof Error) console.error(`Can't open file '${Deno.args[0]}'. Original message: ${e.message}`);
-        console.log('usage: deno run cameltosnake <fileName>')
+      textFileContent = await Deno.readTextFile(fileName);
+    } catch (e: unknown) {
+      if (e instanceof Error) {
+        console.error(
+          `Can't open file '${fileName}'. Original message: ${e.message}`,
+        );
+      }
+      console.log("usage: deno run cameltosnake <fileName>");
     }
-    if(!textFileContent) return;
+    if (!textFileContent) return;
     const regex = /(const|let|var)\s[a-z]+((\d)|([A-Z0-9][a-z0-9]+))+([A-Z])?/g;
-    console.log(`Variables of file '${Deno.args[0]}':`);
-    textFileContent.split('\n').forEach((line, index) => {
-        let match = null;
-        while ((match = regex.exec(line)) != null) {
-            console.log(`line ${index+1}: ${line.substring(match.index, regex.lastIndex).split(' ')[1]}`);
-        }
+    console.log(`Variables of file '${fileName}':`);
+    textFileContent.split("\n").forEach((line, index) => {
+      let match = null;
+      while ((match = regex.exec(line)) != null) {
+        console.log(
+          `line ${index + 1}: ${
+            line.substring(match.index, regex.lastIndex).split(" ")[1]
+          }`,
+        );
+      }
     });
+  }
 })();
